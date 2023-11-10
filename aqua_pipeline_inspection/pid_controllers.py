2eb4cb3e074edbaffdb39a4b8edc3ae8bda1d58a
khalilv <khalil.virji1@gmail.com>
2023-11-10 14:06:06 -0500

    added drift stopping criteria and switched pitch error to depth sensor

--- aqua_pipeline_inspection/pid_controllers.py
+++ aqua_pipeline_inspection/pid_controllers.py
@@ -1,7 +1,8 @@
 import rclpy
 from rclpy.node import Node
-from aqua_pipeline_inspection.control.PID import AnglePID
+from aqua_pipeline_inspection.control.PID import AnglePID, PID
 from aqua2_interfaces.msg import AquaPose, Command
+from std_msgs.msg import Float32
 
 class pid_controllers(Node):
     def __init__(self):
@@ -10,42 +11,49 @@ class pid_controllers(Node):
                                 parameters=[
                                     ('roll_gains', [0.0,0.0,0.0]),
                                     ('pitch_gains', [0.0,0.0,0.0]),
-                                    ('pitch_target', 0.0),
+                                    ('depth_target', 0.0),
                                     ('roll_target', 0.0),
                                     ('speed', 0.0),
                                     
                                 ])
         self.imu_subscriber = self.create_subscription(AquaPose, '/aqua/pose', self.imu_callback, 10)
+        self.depth_subscriber = self.create_subscription(Float32, '/aqua/depth', self.depth_sensor_callback, 10)
         self.command_publisher = self.create_publisher(Command, '/a13/command', 10)
         self.measured_roll_angle = 0.0
-        self.measured_pitch_angle = 0.0
+        self.measured_depth = 0.0
         self.speed = self.get_parameter('speed').get_parameter_value().double_value
         self.roll_gains = self.get_parameter('roll_gains').get_parameter_value().double_array_value
         self.pitch_gains = self.get_parameter('pitch_gains').get_parameter_value().double_array_value
         self.roll_target = self.get_parameter('roll_target').get_parameter_value().double_value
-        self.pitch_target = self.get_parameter('pitch_target').get_parameter_value().double_value
+        self.depth_target = self.get_parameter('depth_target').get_parameter_value().double_value
         self.roll_pid = AnglePID(target = self.roll_target, gains = self.roll_gains, reverse=True)
-        self.pitch_pid = AnglePID(target = self.pitch_target, gains = self.pitch_gains)
+        self.pitch_pid = PID(target = self.depth_target, gains = self.pitch_gains, command_range=[-0.02,0.02], normalization_factor=5)
+
+        #initialize command
+        self.command = Command()
+        self.command.speed = self.speed
+        self.command.roll = 0.0
+        self.command.pitch = 0.0
+        self.command.yaw = 0.0
+        self.command.heave = 0.0
+
         print('Initialized: PID controllers')
 
     def imu_callback(self, imu):
         self.measured_roll_angle = self.calculate_roll(imu)
-        self.measured_pitch_angle = self.calculate_pitch(imu)
-        command = Command();
-        command.speed =self.speed
-        command.pitch = self.pitch_pid.control(self.measured_pitch_angle)
-        command.roll = self.roll_pid.control(self.measured_roll_angle)
-        command.heave = 0.0
-        command.yaw = 0.0
-        self.command_publisher.publish(command)
-        print(command)
-        return command
+        self.command.pitch = self.pitch_pid.control(self.measured_depth)
+        self.command.roll = self.roll_pid.control(self.measured_roll_angle)
+        self.command_publisher.publish(self.command)
+        print(self.command)
+        return
+
+    def depth_sensor_callback(self, depth):
+        self.measured_depth = depth.data
+        return
 
     def calculate_roll(self, imu):
         return imu.roll
     
-    def calculate_pitch(self, imu):
-        return imu.pitch
 
 def main(args=None):
     rclpy.init(args=args)
--- aqua_pipeline_inspection/pid_pipeline_inspection.py
+++ aqua_pipeline_inspection/pid_pipeline_inspection.py
@@ -9,6 +9,8 @@ import os
 class pid_pipeline_inspection(Node):
     def __init__(self):
         super().__init__('pid_pipeline_inspection')
+
+        #subscribers and publishers
         self.command_publisher = self.create_publisher(Command, '/a13/command', 10)
         self.imu_subscriber = self.create_subscription(AquaPose, '/aqua/pose', self.imu_callback, 10)
         self.pipeline_error_subscriber = self.create_subscription(
@@ -16,38 +18,72 @@ class pid_pipeline_inspection(Node):
             '/pipeline/error', 
             self.pipeline_error_callback, 
             10)
+        self.depth_subscriber = self.create_subscription(Float32, '/aqua/depth', self.depth_sensor_callback, 10)
+        
+        #initialize pid controllers
         self.roll_pid = AnglePID(target = 0.0, gains = [2.75, 0.0, 3.75], reverse=True)
-        self.pitch_pid = AnglePID(target = 0.0, gains = [0.5181, 0.0, 0.9])
+        self.pitch_pid = PID(target = 11.0, gains = [0.01, 0.0, 0.25], command_range=[-0.02,0.02], normalization_factor=5)
         self.yaw_pid = PID(target = 0.0, gains = [0.6, 0.0, 1.1], reverse=True, normalization_factor=700)
         self.measured_roll_angle = 0.0
-        self.measured_pitch_angle = 0.0
+        self.measured_depth = 0.0
+        
+        #initialize command
         self.command = Command()
         self.command.speed = 0.0 
         self.command.roll = 0.0
         self.command.pitch = 0.0
         self.command.yaw = 0.0
+        self.command.heave = 0.0
+        
         self.img_size = (300, 400)
+        
+        #trajectory recording
         self.record_trajectory = True
         self.trajectory = []
-        self.finish_line = 25 + 47.14558 #25 + offset
         self.save_path = 'src/aqua_pipeline_inspection/aqua_pipeline_inspection/trajectories/'
         self.num_trajectories = len(os.listdir(self.save_path))
+        
+        #target trajectory
+        self.offset_x = 47.14558
+        self.offset_z = -19.43558
+        self.target_trajectory = '/usr/local/data/kvirji/AQUA/aqua_pipeline_inspection/aqua_pipeline_inspection/trajectories/pipeline_center.npy'
+        with open(self.target_trajectory, 'rb') as f:
+            self.pipeline_x = np.load(f) + self.offset_x
+            self.pipeline_z = np.load(f) + self.offset_z
+        self.max_error_to_target = 3.0 #if < z distance to pipeline then halt
+
+        #end of pipe
+        self.finish_line_x = 25 + self.offset_x #25 + offset
+
         print('Initialized: pid pipeline inspection')
   
     def imu_callback(self, imu):
         self.measured_roll_angle = self.calculate_roll(imu)
-        self.measured_pitch_angle = self.calculate_pitch(imu)
-        if imu.x > self.finish_line:
+        if imu.x > self.finish_line_x:
             self.finish(True)
+        elif np.abs(imu.z - self.get_target_z(imu.x)) > self.max_error_to_target:
+            print('Drifted far from target trajectory')
+            self.finish(False)
         else:
             self.trajectory.append([imu.x, imu.y, imu.z])
+        return
+    
+    def depth_sensor_callback(self, depth):
+        self.measured_depth = depth.data
         return
 
     def calculate_roll(self, imu):
         return imu.roll
     
-    def calculate_pitch(self, imu):
-        return imu.pitch
+    def get_target_z(self, x):
+        ind = np.argwhere(self.pipeline_x >= x)[0][0]
+        x1 = self.pipeline_x[ind - 1]
+        z1 = self.pipeline_z[ind - 1]
+        x2 = self.pipeline_x[ind]
+        z2 = self.pipeline_z[ind]
+        m = (z2 - z1) / (x2 - x1)
+        b = z2 - m * x2
+        return m*x + b
     
     def pipeline_error_callback(self, error):
         if error.data >= self.img_size[0] + self.img_size[1] + 1: #stop command = w + h + 1
@@ -57,7 +93,7 @@ class pid_pipeline_inspection(Node):
             self.command.speed = 0.25 #fixed speed
             self.command.yaw = self.yaw_pid.control(error.data)
             self.command.roll = self.roll_pid.control(self.measured_roll_angle)
-            self.command.pitch = self.pitch_pid.control(self.measured_pitch_angle)
+            self.command.pitch = self.pitch_pid.control(self.measured_depth)
         
         self.command_publisher.publish(self.command)
         return 
