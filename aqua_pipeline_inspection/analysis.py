473d3ae309efbabbe253f99bff31d0aad0197d3a
khalilv <khalil.virji1@gmail.com>
2023-11-13 16:24:15 -0500

    updated analysis to include 3 dimensions

--- aqua_pipeline_inspection/analysis.py
+++ aqua_pipeline_inspection/analysis.py
@@ -1,13 +1,15 @@
 import numpy as np 
 from matplotlib import pyplot as plt
 
-experiment = 2
+experiment = 3
 trajectory_file = '/usr/local/data/kvirji/AQUA/aqua_pipeline_inspection/aqua_pipeline_inspection/trajectories/pid_trajectory_{}.npy'.format(str(experiment))
 save_path = '/usr/local/data/kvirji/AQUA/aqua_pipeline_inspection/aqua_pipeline_inspection/plots/pid_trajectory_{}.png'.format(str(experiment))
 target_trajectory = '/usr/local/data/kvirji/AQUA/aqua_pipeline_inspection/aqua_pipeline_inspection/trajectories/pipeline_center.npy'
 
 offset_x = 47.14558
 offset_z = -19.43558
+offset_y = -99.88829
+target_depth = -10.0
 
 with open(target_trajectory, 'rb') as f:
     pipeline_x = np.load(f) + offset_x
@@ -29,14 +31,21 @@ def get_z_targ(xl, xt, zt):
         z_target.append(m*x + b)
     return z_target
 
-def mse(true,pred):
-    return np.square(np.subtract(true,pred)).mean() 
+def mse(traj_y, traj_z, targ_y, targ_z):
+    return np.square(np.subtract(traj_y,targ_y)).mean() + np.square(np.subtract(traj_z,targ_z)).mean()
 
 xl = trajectory[:,0]
+yl = trajectory[:,1]
 zl = trajectory[:,2]
-target = get_z_targ(xl, pipeline_x, pipeline_z)
 
-plt.plot(xl,target)
-plt.plot(xl,zl)
-plt.title('MSE: ' + str(mse(target, zl)))
+yt = np.zeros(len(xl)) + target_depth
+zt = get_z_targ(xl, pipeline_x, pipeline_z)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot3D(xl, zl, yl, 'blue', label='Trajectory')
+ax.plot3D(xl, zt, yt, 'green', label='Target')
+MSE = mse(yl,zl,yt,zt)
+plt.title('MSE: ' + str(MSE))
+plt.legend()
 plt.show()
