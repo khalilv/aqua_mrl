85650f4fb2a347793831a92a6173bf6a54e2c0e6
khalilv <khalil.virji1@gmail.com>
2024-01-19 13:48:09 -0500

    updated eval

--- aqua_rl/dqn_controller_eval.py
+++ aqua_rl/dqn_controller_eval.py
@@ -10,7 +10,7 @@ from std_msgs.msg import UInt8MultiArray, Float32
 from time import sleep
 from aqua_rl.control.PID import AnglePID
 from aqua_rl.control.DQN import DQN
-from aqua_rl.helpers import define_positive_template, define_negative_template, reward_calculation
+from aqua_rl.helpers import reward_calculation
 from aqua_rl import hyperparams
 
 class dqn_controller_eval(Node):
@@ -35,6 +35,9 @@ class dqn_controller_eval(Node):
         self.eval_episode = hyperparams.eval_episode_
         self.eval_for = hyperparams.eval_for_
         self.detection_threshold = hyperparams.detection_threshold_
+        self.frames_to_skip = hyperparams.frames_to_skip_
+        self.roi_detection_threshold = hyperparams.roi_detection_threshold_
+        self.mean_importance = hyperparams.mean_importance_
 
         #subscribers and publishers
         self.command_publisher = self.create_publisher(Command, '/a13/command', self.queue_size)
@@ -53,6 +56,7 @@ class dqn_controller_eval(Node):
         self.zero_commands = 0
         self.flush_imu = 0
         self.flush_segmentation = 0
+        self.flush_depth = 0
 
         #finished is episode has ended. complete is if aqua has reached the goal 
         self.finished = False
@@ -66,36 +70,26 @@ class dqn_controller_eval(Node):
         self.yaw_actions = np.linspace(-self.yaw_limit, self.yaw_limit, self.yaw_action_space)
         self.pitch_actions = np.linspace(-self.pitch_limit, self.pitch_limit, self.pitch_action_space)
         self.dqn = DQN(int(self.yaw_action_space * self.pitch_action_space), self.history_size) 
+        self.action = torch.tensor([[4]], device=self.dqn.device, dtype=torch.long)
         self.image_history = []
         self.depth_history = []
+        self.action_history = [4]
         self.episode_rewards = []
-
-        #target for reward
-        self.positive_template = define_positive_template(self.img_size)
-        self.negative_template = define_negative_template(self.img_size)
 
         #stopping condition for empty vision input
         self.empty_state_counter = 0
 
-        # self.checkpoint_path = 'src/aqua_rl/experiments/{}/weights/episode_{}.pt'.format(str(self.experiment_number), str(self.eval_episode).zfill(5))
-        # self.save_path = 'src/aqua_rl/evaluations/{}_episode_{}/'.format(str(self.experiment_number), str(self.eval_episode).zfill(5))
-        # if not os.path.exists(self.save_path):
-        #     os.mkdir(self.save_path)
-        # checkpoint = torch.load(self.checkpoint_path, map_location=self.dqn.device)
-        # self.dqn.policy_net.load_state_dict(checkpoint['model_state_dict_policy'], strict=True)
-        # self.dqn.target_net.load_state_dict(checkpoint['model_state_dict_target'], strict=True)
-        # self.dqn.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        # self.dqn.steps_done = checkpoint['training_steps']
-        # self.episode = 0
-        # print('Weights loaded from episode: ', self.eval_episode, ', training steps completed: ', self.dqn.steps_done)
-
-        checkpoint = torch.load('src/aqua_rl/imitation_learning_module/models/best.pt', map_location=self.dqn.device)
-        self.dqn.policy_net.load_state_dict(checkpoint['model_state_dict'], strict=True)
-        self.dqn.target_net.load_state_dict(checkpoint['model_state_dict'], strict=True)
-        self.dqn.optimizer.load_state_dict(checkpoint['optim_state_dict'])
-        self.dqn.steps_done = 0
+        self.checkpoint_path = 'src/aqua_rl/experiments/{}/weights/episode_{}.pt'.format(str(self.experiment_number), str(self.eval_episode).zfill(5))
+        self.save_path = 'src/aqua_rl/evaluations/{}_episode_{}/'.format(str(self.experiment_number), str(self.eval_episode).zfill(5))
+        if not os.path.exists(self.save_path):
+            os.mkdir(self.save_path)
+        checkpoint = torch.load(self.checkpoint_path, map_location=self.dqn.device)
+        self.dqn.policy_net.load_state_dict(checkpoint['model_state_dict_policy'], strict=True)
+        self.dqn.target_net.load_state_dict(checkpoint['model_state_dict_target'], strict=True)
+        self.dqn.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.dqn.steps_done = checkpoint['training_steps']
         self.episode = 0
-        print('Weights loaded from expert')
+        print('Weights loaded from episode: ', self.eval_episode, ', training steps completed: ', self.dqn.steps_done)
 
         #initialize command
         self.command = Command()
@@ -127,29 +121,42 @@ class dqn_controller_eval(Node):
         self.measured_roll_angle = self.calculate_roll(imu)
 
         if imu.x > self.finish_line_x:
+            print('Reached finish line')
             self.flush_commands = 0
             self.finished = True
             self.complete = True
-        elif imu.x < self.start_line_x:
+        if imu.x < self.start_line_x:
+            print('Tracked backwards to starting line')
             self.flush_commands = 0
             self.finished = True
             self.complete = True
-        elif imu.y < self.depth_range[1]:
-            print('Drifted close to seabed')
-            self.flush_commands = 0
-            self.finished = True
-            self.complete = False
-        elif imu.y > self.depth_range[0]:
-            print('Drifted far above target')
-            self.flush_commands = 0
-            self.finished = True
-            self.complete = False
         else:
             self.trajectory.append([imu.x, imu.y, imu.z])
         return
     
     def depth_callback(self, depth):
-        self.relative_depth = self.target_depth + depth.data
+        
+        #finished flag
+        if self.finished:
+            return
+        
+        #flush queue
+        if self.flush_depth < self.flush_steps:
+            self.flush_depth += 1
+            return
+        
+        if -depth.data < self.depth_range[1]:
+            print('Drifted close to seabed')
+            self.flush_commands = 0
+            self.finished = True
+            self.complete = False
+        elif -depth.data > self.depth_range[0]:
+            print('Drifted far above target')
+            self.flush_commands = 0
+            self.finished = True
+            self.complete = False
+        else:
+            self.relative_depth = self.target_depth + depth.data
         return
     
     def calculate_roll(self, imu):
@@ -185,49 +192,51 @@ class dqn_controller_eval(Node):
             return
 
         seg_map = np.array(seg_map.data).reshape(self.img_size)
-        if len(self.image_history) < self.history_size:
-            self.depth_history.append(self.relative_depth)
-            self.image_history.append(seg_map)
+        #check for empty input from vision module
+        if seg_map.sum() < self.detection_threshold:
+            self.empty_state_counter += 1
         else:
-            self.image_history.pop(0)
-            self.image_history.append(seg_map)
-            self.depth_history.pop(0)
-            self.depth_history.append(self.relative_depth)
+            self.empty_state_counter = 0
+        
+        #if nothing has been detected in empty_state_max frames then reset
+        if self.empty_state_counter >= self.empty_state_max:
+            print("Nothing detected in state space for {} states".format(str(self.empty_state_max)))
+            self.flush_commands = 0
+            self.finished = True
+            self.complete = False
+            return
+        
+        self.depth_history.append(self.relative_depth)
+        self.image_history.append(seg_map)
+        if len(self.image_history) == self.history_size and len(self.depth_history) == self.history_size and len(self.action_history) == self.history_size:
             s = np.array(self.image_history)
             sd = np.array(self.depth_history)
-            
-            #check for empty input from vision module
-            if s.sum() < self.detection_threshold:
-                self.empty_state_counter += 1
-            else:
-                self.empty_state_counter = 0
-            
-            #if nothing has been detected in empty_state_max frames then reset
-            if self.empty_state_counter >= self.empty_state_max:
-                print("Nothing detected in state space for {} states".format(str(self.empty_state_max)))
-                self.flush_commands = 0
-                self.finished = True
-                self.complete = False
-                return
+            sa = np.array(self.action_history)
             
             state = torch.tensor(s, dtype=torch.float32, device=self.dqn.device).unsqueeze(0)
             state_depths = torch.tensor(sd, dtype=torch.float32, device=self.dqn.device).unsqueeze(0)
+            state_actions = torch.tensor(sa, dtype=torch.float32, device=self.dqn.device).unsqueeze(0)
 
-            reward = reward_calculation(seg_map, self.relative_depth, self.positive_template, self.negative_template, self.detection_threshold)
+            reward = reward_calculation(seg_map, self.relative_depth, self.roi_detection_threshold, self.mean_importance)
             self.episode_rewards.append(reward)
-            action = self.dqn.select_eval_action(state, state_depths)
+            self.action = self.dqn.select_eval_action(state, state_depths, state_actions)
 
-            action_idx = action.detach().cpu().numpy()[0][0]
-            self.command.pitch = self.pitch_actions[int(action_idx/self.yaw_action_space)]
-            self.command.yaw = self.yaw_actions[action_idx % self.yaw_action_space]            
-            self.command.speed = hyperparams.speed_ #fixed speed
-            self.command.roll = self.roll_pid.control(self.measured_roll_angle)
-            self.command_publisher.publish(self.command)
+            self.image_history = self.image_history[self.frames_to_skip:]
+            self.depth_history = self.depth_history[self.frames_to_skip:]
+            self.action_history = self.action_history[self.frames_to_skip:]
+        
+        action_idx = self.action.detach().cpu().numpy()[0][0]
+        self.action_history.append(action_idx)
+        self.command.pitch = self.pitch_actions[int(action_idx/self.yaw_action_space)]
+        self.command.yaw = self.yaw_actions[action_idx % self.yaw_action_space]            
+        self.command.speed = hyperparams.speed_ #fixed speed
+        self.command.roll = self.roll_pid.control(self.measured_roll_angle)
+        self.command_publisher.publish(self.command)
         return 
 
     def finish(self):
+        
         if self.complete:
-            print('Goal reached')
             reward = hyperparams.goal_reached_reward_
             self.episode_rewards.append(reward)
         else:
@@ -237,7 +246,6 @@ class dqn_controller_eval(Node):
         self.episode_rewards = np.array(self.episode_rewards)
         print('Episode rewards. Average: ', np.mean(self.episode_rewards), ' Sum: ', np.sum(self.episode_rewards))
 
-        print('Saving trajectory')
         with open(self.save_path + '/episode_{}.npy'.format(str(self.episode).zfill(5)), 'wb') as f:
             np.save(f, self.episode_rewards)
             np.save(f, np.array(self.trajectory))
@@ -282,12 +290,15 @@ class dqn_controller_eval(Node):
         #reset history queue
         self.image_history = []
         self.depth_history = []
+        self.action_history = [4]
+        self.action = torch.tensor([[4]], device=self.dqn.device, dtype=torch.long)
 
         #reset flush queues 
         self.flush_commands = self.flush_steps
         self.zero_commands = 0
         self.flush_imu = 0
         self.flush_segmentation = 0
+        self.flush_depth = 0
 
         #reset counters
         self.empty_state_counter = 0
